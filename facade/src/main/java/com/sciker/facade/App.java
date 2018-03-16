263fbdb7ad343d69729737a44aa382caaa5a7602
Hitesh Wadekar <hitesh@Hiteshs-MacBook-Pro.local>
2018-03-16 11:08:22 -0700

    Initailizing jdesign maven project structures

---
+++ facade/src/main/java/com/sciker/facade/App.java
@@ -0,0 +1,20 @@
+package com.sciker.facade;
+
+/**
+ * This is facade design pattern.
+ *
+ *
+ */
+
+public class App {
+  /**
+   *
+   * Program entry section.
+   *
+   * @param args
+   */
+
+  public static void main(String[] args){
+    System.out.println("\n Welcome to facade design pattern ");
+  }
+}
