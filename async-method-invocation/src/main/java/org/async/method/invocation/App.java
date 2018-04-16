c6af8dc4ee7e678cdd7d87173c31fb1bed7e7f00
Hitesh <hitesh@Hiteshs-MacBook-Pro.local>
2018-04-15 18:33:34 -0700

    UPdated code for facade design patter

---
+++ async-method-invocation/src/main/java/org/async/method/invocation/App.java
@@ -0,0 +1,13 @@
+package org.async.method.invocation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "\n Welcome to async method pattern " );
+    }
+}
